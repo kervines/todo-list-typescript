485ce2445cd72b71ad891c365b524e7de2217840
kervines <kervinzero@gmail.com>
2024-07-11 22:24:55 -0300

    feat: add code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-alert("Bora iniciar o typeScript")
+(() => {
+  const todo = {
+    description: 'todo',
+    done: false,
+  };
+
+  const reminder = {
+    description: 'reminder',
+    date: '11.07.2024',
+  };
+
+  const taskView = {
+    render(tasks: Array<Object>) {
+      const tasksList = document.getElementById('tasksList');
+      while (tasksList?.firstChild) {
+        tasksList.removeChild(tasksList.firstChild);
+      }
+
+      tasks.forEach((task) => {
+        const li = document.createElement('li');
+        const textNode = document.createTextNode(JSON.stringify(task));
+        li.appendChild(textNode);
+        tasksList?.appendChild(li);
+      });
+    },
+  };
+
+  const TaskController = (view: typeof taskView) => {
+    const tasks: Array<Object> = [todo, reminder];
+
+    const handleEvent = (event: Event) => {
+      event.preventDefault();
+      view.render(tasks);
+    };
+    document
+      .getElementById('taskForm')
+      ?.addEventListener('submit', handleEvent);
+  };
+
+  TaskController(taskView);
+})();
